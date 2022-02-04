4ff43b5827af0dae3c35559ad82db42d2a5278b0
pallagel <lalith.pallage77@outlook.com>
2022-02-03 21:15:46 -0700

    Program to check if user input is a Palindrome

---
+++ 02-StringPalindrome/main.go
@@ -0,0 +1,29 @@
+//Author - Lalith Pallage
+//Check if a string is a Palindrome
+
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+
+	//Accept user input
+	fmt.Print("Enter a string : ")
+	fmt.Scanln(&input)
+
+	fmt.Println(IsPalindrome(input))
+}
+
+//IsPalindrome - function to test a string is a Palindrome
+func IsPalindrome(input string) bool {
+	j := len(input) //sset the variable j to length of the string
+
+	//loop through input mid-length and check characters at begging and end to see if they are the same
+	for i := 0; i < j/2; i++ {
+		if (input[i]) != input[j-1] {
+			return false
+		}
+	}
+	return true
+}
